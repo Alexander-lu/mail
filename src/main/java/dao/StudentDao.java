c72e60bd9e01ff9799652b67091340ff822c0726
Alexander_Lu <81604280@qq.com>
2022-06-27 22:54:44 +0800

    增删改查修订

--- src/main/java/dao/StudentDao.java
+++ src/main/java/dao/StudentDao.java
@@ -10,15 +10,15 @@ import java.util.List;
 @Mapper
 public interface StudentDao {
     //增
-    @Update("INSERT into student(username,password) VALUES(#{username},#{password});")
+    @Update("INSERT into user(username,password) VALUES(#{username},#{password});")
     public boolean insertStudent(Student student);
 
     //删
-    @Update("delete from student where username=#{username};")
+    @Update("delete from user where username=#{username};")
     public boolean deleteStudent(String username);
 
     //改
-    @Update("update student set username=#{username},password=#{password} where username=#{username};")
+    @Update("update user set username=#{username},password=#{password} where username=#{username};")
     public boolean updateStudent(Student student);
 
     //查
