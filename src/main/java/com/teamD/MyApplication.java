733bf2e3fd665b3df26316ee70a60d9008cc984e
Alexander_Lu <81604280@qq.com>
2022-06-30 02:48:05 +0800

    添加了注释

--- src/main/java/com/teamD/MyApplication.java
+++ src/main/java/com/teamD/MyApplication.java
@@ -6,7 +6,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.sql.DataSource;
 /**
- *myApplication会自动扫描com.teadD包下的所有java文件
+ * SpringBootApplication注解表示开启组件扫描和自动配置
+ *组件扫描会自动扫描com.teadD包下的所有java文件
+ * 自动配置会自动配置所有Configuration类
+ * SpringBootApplication注解里包含了Configuration，ComponentScan和EnableAutoConfuguration这3个注解
+ * Configuration表明该类属于配置类
+ * ComponentScan表明扫描类
+ * EnableAutoConfuguration开启了自动配置
  * @author teamD
  * @version 1.0
  * 20220628
