2a09b1395820eee19c6409775a62a55958fdd231
Alexander_Lu <81604280@qq.com>
2022-06-30 03:03:51 +0800

    添加了注释

--- spa/vue.config.js
+++ spa/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     devServer: {
       proxy: {
         // 前后端分离开发，调试时为解决跨域问题，利用webpack配置一个简单的反向代理。见：
+        //  pathRewrite: { '^/api': '' }表示访问后端的时候把api这个后缀隐藏
         // https://cli.vuejs.org/guide/webpack.html
         // https://webpack.js.org/configuration/dev-server/#devserverproxy
         '/api': {
