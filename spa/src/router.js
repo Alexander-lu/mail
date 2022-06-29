c5974968b6c6e8a4f923d716d8a83eff725005b5
Alexander_Lu <81604280@qq.com>
2022-06-30 05:06:40 +0800

    开发了main页面

--- spa/src/router.js
+++ spa/src/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Register from './views/Register'
 import Login from './views/Login'
+import Main from './views/main'
+import Error403 from './views/403.vue'
+import Error404 from './views/404.vue'
+import permission from './views/Permission.vue'
 Vue.use(Router)
 export default new Router({
     routes: [
@@ -11,13 +15,33 @@ export default new Router({
             component: Register
         },
         {
+            path: '/403',
+            name: '403',
+            component: Error403
+        },
+        {
+            path: '/permission',
+            name: 'Permission',
+            component: permission
+        },
+        {
+            path: '/404',
+            name: '404',
+            component: Error404
+        },
+        {
             path: '/login',
             name: 'Login',
             component: Login
         },
         {
+            path: '/main',
+            name: 'Main',
+            component: Main
+        },
+        {
             path: '*',
-            redirect: '/register'
+            redirect: '/main'
         }
     ]
 });
--- spa/src/views/Login.vue
+++ spa/src/views/Login.vue
@@ -4,8 +4,6 @@
   <el-main>
   <el-input  placeholder="请输入账号" v-model="username" clearable></el-input>
  <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
-  <el-input  placeholder="请输入邮箱地址" v-model="mail"  clearable></el-input>
-  <el-button type="primary" icon="el-icon-thumb">点我注册</el-button>
   </el-main>
   <el-footer>Footer</el-footer>
 </el-container>
